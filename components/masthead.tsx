f5befe546be0c3f5ae221a87c8e58d2cd661cc8d
Anna Sakurai <zenasak@gmail.com>
2022-07-14 09:43:40 +0200

    add logo

--- components/masthead.tsx
+++ components/masthead.tsx
@@ -13,10 +13,18 @@ const Masthead: React.FC = () => {
       >
         <source src="/assets/geometric.mp4" type="video/mp4" />
       </video>
+      <div className={`flex-grow-0 pt-10 transition-opacity duration-1000`}>
+        <Image
+          src="/assets/logo.svg"
+          width={123 / 3}
+          height={114 / 3}
+          alt="logo"
+        />
+      </div>
       <div className="p-12 font-bold z-10 text-white drop-shadow-[0_5px_3px_rgba(0,0,0,0.4)] text-center flex-1 flex items-center justify-center flex-col">
-        <h1 className="mb-6 text-4xl xl:text-5xl">Margelo</h1>
+        <h1 className="mb-6 text-4xl xl:text-5xl">Chika & Tatsuya</h1>
         <h2 className="mb-2 text-2xl xl:text-3xl tracking-tight">
-          <span>App Development,</span> <span>done right.</span>
+          <span>Jazz Duo</span> <span>base in NY</span>
         </h2>
       </div>
     </div>
