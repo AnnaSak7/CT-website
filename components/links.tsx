6aa0a7003a331936ce1ad347c1c905f95d050b16
Anna Sakurai <zenasak@gmail.com>
2022-07-17 12:42:28 +0200

    add icons

--- components/links.tsx
+++ components/links.tsx
@@ -1,10 +1,55 @@
 import React from "react";
 import Carousel from "./carousel";
 import CarouselItem from "./carousel-item";
+import Image from "next/image";
 
 const Links: React.FC = () => (
-  <Carousel className="bg-black text-white py-10 lg:py-20">
-    <CarouselItem></CarouselItem>
+  <Carousel className="py-10 lg:py-20">
+    <CarouselItem index={0}>
+      <Image
+        src="/assets/social/instagram.png"
+        width={50}
+        height={50}
+        alt="logo"
+        objectFit="contain"
+      />
+    </CarouselItem>
+    <CarouselItem index={0}>
+      <Image
+        src="/assets/social/youtube.png"
+        width={50}
+        height={50}
+        alt="logo"
+        objectFit="contain"
+      />
+    </CarouselItem>
+    <CarouselItem index={0}>
+      <Image
+        src="/assets/social/twitch.png"
+        width={50}
+        height={50}
+        alt="logo"
+        objectFit="contain"
+      />
+    </CarouselItem>
+    <CarouselItem index={0}>
+      <Image
+        src="/assets/social/discord.png"
+        width={50}
+        height={50}
+        alt="logo"
+        objectFit="contain"
+      />
+    </CarouselItem>
+    <CarouselItem index={0}>
+      <Image
+        src="/assets/social/twitter.png"
+        width={50}
+        height={50}
+        alt="logo"
+        objectFit="contain"
+      />
+    </CarouselItem>
   </Carousel>
 );
 
