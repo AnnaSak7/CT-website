1cb48259963768127596e5e9cf76d825409c0b81
Anna Sakurai <zenasak@gmail.com>
2022-07-22 20:29:44 +0200

    fix social media icons with links

--- components/photos.tsx
+++ components/photos.tsx
@@ -7,59 +7,56 @@ import { WorkLink } from "./work";
 const Photos: React.FC = () => (
   <Carousel className="py-10 lg:py-20">
     <CarouselItem index={0}>
-      <WorkLink href="https://www.instagram.com/monchika1231/?hl=ja">
-        <Image
-          src="/assets/photos/01.png"
-          width={500}
-          height={200}
-          alt="logo"
-          objectFit="contain"
-        />
-      </WorkLink>
+      {/* <WorkLink href="https://www.instagram.com/monchika1231/?hl=ja"> */}
+
+      <Image
+        src="/assets/photos/01.png"
+        width={1000}
+        height={600}
+        alt="chika&tatsuya photo"
+        objectFit="contain"
+      />
+      {/* </WorkLink> */}
     </CarouselItem>
     <CarouselItem index={1}>
-      <WorkLink href="https://www.youtube.com/user/chikatanaka12311">
-        <Image
-          src="/assets/photos/02.png"
-          width={500}
-          height={200}
-          alt="logo"
-          objectFit="contain"
-        />
-      </WorkLink>
+      <Image
+        src="/assets/photos/02.png"
+        width={1000}
+        height={600}
+        alt="chika&tatsuya photo"
+        objectFit="contain"
+      />
     </CarouselItem>
     <CarouselItem index={2}>
-      <WorkLink href="https://www.twitch.tv/chika_tatsuya">
-        <Image
-          src="/assets/photos/03.png"
-          width={500}
-          height={200}
-          alt="logo"
-          objectFit="contain"
-        />
-      </WorkLink>
+      <Image
+        src="/assets/photos/03.png"
+        width={1000}
+        height={600}
+        alt="chika&tatsuya photo"
+        objectFit="contain"
+      />
     </CarouselItem>
     <CarouselItem index={3}>
-      <WorkLink href="https://discord.com/invite/2dnCVETE8w">
-        <Image
-          src="/assets/photos/04.png"
-          width={500}
-          height={200}
-          alt="logo"
-          objectFit="contain"
-        />
-      </WorkLink>
+      {/* <WorkLink href="https://discord.com/invite/2dnCVETE8w"> */}
+      <Image
+        src="/assets/photos/04.png"
+        width={1000}
+        height={600}
+        alt="chika&tatsuya photo"
+        objectFit="contain"
+      />
+      {/* </WorkLink> */}
     </CarouselItem>
     <CarouselItem index={4}>
-      <WorkLink href="https://twitter.com/ChikaTanaka10">
-        <Image
-          src="/assets/photos/04.png"
-          width={500}
-          height={200}
-          alt="logo"
-          objectFit="contain"
-        />
-      </WorkLink>
+      {/* <WorkLink href="https://twitter.com/ChikaTanaka10"> */}
+      <Image
+        src="/assets/photos/04.png"
+        width={1000}
+        height={600}
+        alt="chika&tatsuya photo"
+        objectFit="contain"
+      />
+      {/* </WorkLink> */}
     </CarouselItem>
   </Carousel>
 );
--- components/social-logos.tsx
+++ components/social-logos.tsx
@@ -1,48 +1,57 @@
 import React from "react";
 import Image from "next/image";
 import SliderContainer, { SliderItem } from "./slider";
+import { WorkLink } from "./work";
 
 const SocialLogos: React.FC = () => (
-  <>
-    <SliderContainer className="" contentWidth={1290} initialOffsetX={0}>
-      <SliderItem width={100}>
-        <Image
-          src="/assets/social/discord.png"
-          width={100}
-          height={50}
-          alt="logo"
-          objectFit="contain"
-        />
-      </SliderItem>
-      <SliderItem width={100}>
-        <Image
-          src="/assets/social/twitch.png"
-          width={100}
-          height={50}
-          alt="logo"
-          objectFit="contain"
-        />
-      </SliderItem>
-      <SliderItem width={100}>
-        <Image
-          src="/assets/social/youtube.png"
-          width={100}
-          height={50}
-          alt="logo"
-          objectFit="contain"
-        />
-      </SliderItem>
-      <SliderItem width={100}>
-        <Image
-          src="/assets/social/twitter.png"
-          width={100}
-          height={50}
-          alt="logo"
-          objectFit="contain"
-        />
-      </SliderItem>
-    </SliderContainer>
-  </>
+  <div className="flex">
+    {/* <SliderContainer className="" contentWidth={1290} initialOffsetX={0}> */}
+    {/* <SliderItem width={100}> */}
+    <WorkLink href="https://discord.com/invite/2dnCVETE8w">
+      <Image
+        src="/assets/social/discord.png"
+        width={100}
+        height={50}
+        alt="logo"
+        objectFit="contain"
+      />
+    </WorkLink>
+    {/* </SliderItem> */}
+    {/* <SliderItem width={100}> */}
+    <WorkLink href="https://www.twitch.tv/chika_tatsuya">
+      <Image
+        src="/assets/social/twitch.png"
+        width={100}
+        height={50}
+        alt="logo"
+        objectFit="contain"
+      />
+    </WorkLink>
+    {/* </SliderItem> */}
+    {/* <SliderItem width={100}> */}
+    <WorkLink href="https://www.youtube.com/user/chikatanaka12311">
+      <Image
+        src="/assets/social/youtube.png"
+        width={100}
+        height={50}
+        alt="logo"
+        objectFit="contain"
+      />
+    </WorkLink>
+    {/* </SliderItem> */}
+    {/* <SliderItem width={100}> */}
+    <WorkLink href="https://twitter.com/ChikaTanaka10">
+      <Image
+        src="/assets/social/twitter.png"
+        width={100}
+        height={50}
+        alt="logo"
+        objectFit="contain"
+      />
+    </WorkLink>
+    {/* </SliderItem> */}
+    {/* </SliderContainer> */}
+  </div>
 );
 
 export default SocialLogos;
