f3c5355ba86f1bbd04410005c451e897fabdde66
Anna Sakurai <zenasak@gmail.com>
2022-07-22 20:15:45 +0200

    add copyright

--- components/footer.tsx
+++ components/footer.tsx
@@ -5,8 +5,7 @@ import Image from "next/image";
 const Footer: React.FC = () => (
   <footer className="flex gap-8 items-center justify-center bg-black text-white p-20">
     <Image src="/assets/logo.svg" width={18} height={18} alt="logo" />
-    <Link href="/terms">Terms</Link>
-    <Link href="/privacy">Privacy Policy</Link>
+    <p>&copy; Copyright 2022 Chika & Tatsuya</p>
   </footer>
 );
 export default Footer;
